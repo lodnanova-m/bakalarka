0c341c4e02ddc5d7cf09bd4466f6365786eca7b7
lodnanova-m <525221@mail.muni.cz>
2024-02-22 22:33:59 +0100

    fix: check response

--- next_js_app/src/app/store/page.tsx
+++ next_js_app/src/app/store/page.tsx
@@ -7,11 +7,17 @@ async function getProducts() {
     method: "GET",
     next: { revalidate: 10 },
   });
+  if (response.status !== 200) {
+    return null;
+  }
   return response.json() as Promise<{ products: Product[] }>;
 }
 
 export default async function Store() {
   const response = await getProducts();
+  if (!response) {
+    return <div>Failed to load products</div>;
+  }
   return (
     <div className={styles.container}>
       <ProductList items={response.products}></ProductList>
