921881ac9ac5aa133bf954342f58c82481bf1d2a
Michaela Lodňanová <525221@mail.muni.cz>
2024-05-03 21:29:28 +0200

    fix: angular express server

--- angular-app/server.ts
+++ angular-app/server.ts
@@ -28,7 +28,7 @@ export function app(): express.Express {
   );
 
   // All regular routes use the Angular engine
-  server.get('/products', (req, res, next) => {
+  server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
     commonEngine
       .render({
