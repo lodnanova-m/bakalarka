75f6768020b3d0351c6c07e828c2d41cc1eecd25
Michaela Lodňanová <525221@mail.muni.cz>
2024-05-03 21:05:32 +0200

    fix: project name in postbuild script

--- angular-app/bin/postbuild.sh
+++ angular-app/bin/postbuild.sh
@@ -4,8 +4,8 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp -r ./dist/your-project/server ./.amplify-hosting/compute/default
+cp -r ./dist/angular-app/server ./.amplify-hosting/compute/default
 
-cp -r ./dist/your-project/browser ./.amplify-hosting/static
+cp -r ./dist/angular-app/browser ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
