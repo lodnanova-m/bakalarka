10f8367e6d4f301a78287696f1af17bea6eeb83b
Michaela Lodňanová <525221@mail.muni.cz>
2024-05-21 00:07:05 +0200

    fix: ciarky

--- angular-app/src/environments/environment.development.ts
+++ angular-app/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://master.d3d95ljo92pafp.amplifyapp.com/',
+  apiUrl: 'https://master.d3d95ljo92pafp.amplifyapp.com',
 };
--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://master.d3d95ljo92pafp.amplifyapp.com/',
+  apiUrl: 'https://master.d3d95ljo92pafp.amplifyapp.com',
 };
