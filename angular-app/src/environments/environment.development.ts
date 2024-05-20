561cc17fe16cb467324c576206c6cedb6034cf17
Michaela Lodňanová <525221@mail.muni.cz>
2024-05-21 00:03:24 +0200

    fix: server

--- angular-app/src/environments/environment.development.ts
+++ angular-app/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://master.d3d95ljo92pafp.amplifyapp.com/',
 };
--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://master.d3d95ljo92pafp.amplifyapp.com/',
 };
